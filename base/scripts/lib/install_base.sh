c9e9ba35ca82e86d735babb65fa97a166b3f98ae
bingo <shaw_hank@163.com>
2015-10-15 15:21:04 +0800

    new

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential python git
+apt-get install -y curl wget bzip2 build-essential python git
 
 #locale-gen en_US.UTF-8
--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -6,7 +6,7 @@ PHANTOMJS_VERSION=1.9.8
 PHANTOMJS_TAR_FILE=phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 
 cd /usr/local/share/
-curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 tar xjf $PHANTOMJS_TAR_FILE
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
